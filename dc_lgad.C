c2eef08c094d9de1c47b65c702b69a66a95144de
张阳帆 <yangfanzhang@MacBook-Air-42.local>
2021-12-09 20:53:24 +0800

    update

--- dc_lgad.C
+++ dc_lgad.C
@@ -24,6 +24,7 @@
 
 using namespace Garfield;
 
+// define transfer function
 double transfer(double t) {
 	constexpr double tauR = 5.6;
 	constexpr double tauI = 1.8;
@@ -47,17 +48,28 @@ int main(int argc, char * argv[]) {
 	
 	TApplication app("app", &argc, argv);
 
+// define constants
 	const double depth = 50.e-4;
 	const double left = 175.e-4;
 	const double right = 1125.e-4;
 	const double width = 650.e-4;
 
+// define flags
+	constexpr bool plotField = false;
+  constexpr bool plotWeightingField = false;
+	constexpr bool plotSignal = false;
+	constexpr bool plotDrift = false;
+  constexpr bool writeSignal = false;
+	constexpr bool writeTime = true;
+	constexpr bool useTransfer = true;
+
 	MediumSilicon si;
 	ComponentTcad2d cmp;
-//  cmp.EnableDebugging();
+//  cmp.EnableDebugging(); // open debug mode
   cmp.Initialise("n3397_des.grd", "n3397_des.dat");
 	cmp.SetRangeZ(-width, width);
 
+// assign silicon to corresponding region
 	int nRegions = cmp.GetNumberOfRegions();
 	for (int i = 0; i < nRegions; ++i) {
 		std::string region;
@@ -65,27 +77,22 @@ int main(int argc, char * argv[]) {
 		cmp.GetRegion(i, region, active);
 		if (region == "\"Silicon_1\"") cmp.SetMedium(i, &si);
 	}
-	
-/*
-	ComponentAnalyticField wfieldAnalytic;
-	wfieldAnalytic.AddPlaneY(0., 0., "top");
-	wfieldAnalytic.AddPlaneY(depth, -500., "bot");
-	wfieldAnalytic.AddStripOnPlaneY('z', depth, 0., width, "strip");
-	wfieldAnalytic.AddReadout("strip");
-*/
+
+// add electrodes for producing the corresponding weightfield	
 	ComponentAnalyticField wfieldAnalytic;
 	wfieldAnalytic.AddPlaneX(0., 0., "top");
 	wfieldAnalytic.AddPlaneX(depth, -100, "bot");
 	wfieldAnalytic.AddStripOnPlaneX('z', 0., left, right, "strip");
 	wfieldAnalytic.AddReadout("strip");
 
+// view potential field	
 	ViewField vField;
-	constexpr bool plotField = false;
 	if (plotField) {
 		vField.SetComponent(&cmp);
 		vField.PlotContour("v");
 	}
-  constexpr bool plotWeightingField = false;
+
+// view weightfield	
   if (plotWeightingField) {
     ViewField* wfieldView = new ViewField();
     wfieldView->SetComponent(&wfieldAnalytic);
@@ -99,12 +106,15 @@ int main(int argc, char * argv[]) {
 	
 	const std::string label = "strip";
 
-	const double endtime = 20;
-//	const double endtime = 5;
+	if (useTransfer) {
+		const double endtime = 20;
+		sensor.SetTransferFunction(transfer);
+	} else {
+		const double endtime = 5;
+	}
 	const int nSignalBins = 100000;
 	double tStep = endtime / nSignalBins;
 	sensor.SetTimeWindow(0., tStep, nSignalBins);
-	sensor.SetTransferFunction(transfer);
 
 	const double thr1 = -1000 * ElementaryCharge;
 	std::cout << "Threshold: " << thr1 << " fC\n";
@@ -118,22 +128,20 @@ int main(int argc, char * argv[]) {
 	TrackHeed track;
 	track.SetSensor(&sensor);
 	track.SetParticle("pi");
-	track.SetMomentum(420.e6);
+	track.SetMomentum(420.e6); // MIP
 
+// view signal
 	ViewSignal vSignal;
-	constexpr bool plotSignal = false;
 	vSignal.SetSensor(&sensor);
 	
-	constexpr bool writeSignal = false;
-	constexpr bool writeTime = true;
-
+// view drift lines	
 	ViewDrift vDrift;
-	constexpr bool plotDrift = false;
 	if (plotDrift) {
 		vDrift.SetArea(0., 0., depth, 1300.e-4);
 		track.EnablePlotting(&vDrift);
 	}
 
+// open files	
 	std::ofstream output_charge, output_time, output_gain;
 	std::ofstream log;
 	output_charge.open("charge_lgad.dat", std::ios::out);
@@ -141,6 +149,7 @@ int main(int argc, char * argv[]) {
 	output_gain.open("gain.dat", std::ios::out);
 	log.open("log_lgad", std::ios::out);
 
+// signal simulation	
 	const unsigned int nEvents = 500;
 	for (unsigned int j = 0; j < nEvents; ++j) {
 		sensor.ClearSignal();
@@ -155,8 +164,8 @@ int main(int argc, char * argv[]) {
 		std::cout << "No." << j << "\n";
 		while (track.GetCluster(xc, yc, zc, tc, ne, nh, ec, dummy)) {
 			++nc;
-			nesum += ne;
-			nesum_real += ne;
+			nesum += ne; // primary electron number
+			nesum_real += ne; // total electron number
 			if (nc % 100 == 0) std::cout << "		Cluster " << nc << "\n";
 			drift.DisablePlotting();
 			if (plotDrift && RndmUniform() < 0.05) {
@@ -165,20 +174,16 @@ int main(int argc, char * argv[]) {
 			double xe, ye, ze, te, ee, dxe, dye, dze;
 			for (int i = 0; i < ne; ++i) {
 				track.GetElectron(i, xe, ye, ze, te, ee, dxe, dye, dze);
-//				drift.DriftElectron(xe, ye, ze, te);
+//				drift.DriftElectron(xe, ye, ze, te); // for non-avalanche case
 				drift.AvalancheElectronHole(xe, ye, ze, te);
 				int size = drift.GetNumberOfElectronEndpoints();
 				log << "size = " << size << "\n";
 				nesum_real += size - 1;
-				doublecheck += size;
+				doublecheck += size; // another way of counting total electrons
 				if (size > 1) log << "avalanche with " << size << "\n";
 			}
-			double xh, yh, zh, th;
-/*			for (int i = 0; i < nh; ++i) {
-				track.GetIon(i, xh, yh, zh, th);
-				drift.DriftHole(xh, yh, zh, th);
-			}*/
 		}
+
 		std::cout << nesum << " electrons, " << nesum * ElementaryCharge << " fC.\n";
 //		std::cout << nesum_real << " electrons, " << nesum_real * ElementaryCharge << " fC.\n";
 		std::cout << doublecheck << " electrons, " << doublecheck * ElementaryCharge << " fC.\n";
@@ -189,6 +194,7 @@ int main(int argc, char * argv[]) {
 		output_charge << nesum_real * ElementaryCharge << "\n";
 		sensor.ConvoluteSignals();
 
+// write signals to files
 		if (writeSignal) {
 			char filename[50];
 			sprintf(filename, "signal_lgad_%05d.txt", j);
@@ -204,11 +210,13 @@ int main(int argc, char * argv[]) {
 			outfile.close();
 		}
 
+// write time info		
 		if (writeTime) {
 			std::cout << "write time" << "\n";
 			double min = 999;
 			double time = 0;
 			double min_time = 0;
+			// loop for peak value and corresponding time
 			for (unsigned int i = 0; i < nSignalBins; ++i) {
 				const double f = sensor.GetSignal(label, i);
 				const double t = (i + 0.5) * tStep;
@@ -217,6 +225,7 @@ int main(int argc, char * argv[]) {
 					min_time = t;
 				}
 			}
+			// find the time stamp of half peak value in the rising period
 			for (unsigned int i = 0; i < nSignalBins; ++i) {
 				const double f = sensor.GetSignal(label, i);
 				const double t = (i + 0.5) * tStep;
@@ -224,38 +233,7 @@ int main(int argc, char * argv[]) {
 			}
 			output_time << time << "	" << min << "\n";
 		}
-  /*
-	AvalancheMicroscopic aval;
-  aval.SetSensor(&sensor);
 
-  for (unsigned int i = 0; i < nEvents; ++i) { 
-    std::cout << i << "/" << nEvents << "\n";
-    // Randomize the initial position. 
-    const double x0 = 0;
-    const double y0 = 650.e-4;
-    const double z0 = 0.; 
-    const double t0 = 0.;
-    const double e0 = 0.1;
-    aval.AvalancheElectron(x0, y0, z0, t0, e0, 0.1, 0., 0.);
-    int ne = 0, ni = 0;
-    aval.GetAvalancheSize(ne, ni);
-    const unsigned int np = aval.GetNumberOfElectronEndpoints();
-    double xe1, ye1, ze1, te1, e1;
-    double xe2, ye2, ze2, te2, e2;
-    double xi1, yi1, zi1, ti1;
-    double xi2, yi2, zi2, ti2;
-    int status;
-    for (unsigned int j = 0; j < np; ++j) {
-      aval.GetElectronEndpoint(j, xe1, ye1, ze1, te1, e1, 
-                                  xe2, ye2, ze2, te2, e2, status);
-      drift.DriftHole(xe1, ye1, ze1, te1);
-      drift.GetHoleEndpoint(0, xi1, yi1, zi1, ti1, 
-                              xi2, yi2, zi2, ti2, status);
-    }
-		if (!plotSignal) continue;
-		vSignal.PlotSignal("strip");
-  }
-*/
 //		if (!plotSignal) continue;
 //		vSignal.PlotSignal("strip");
 	}
@@ -270,6 +248,6 @@ int main(int argc, char * argv[]) {
 		vDrift.Plot(twod, true);
 	}
 
-//	app.Run(true);
+//	app.Run(true); // if open, root will need to be closed manually
 
 }
